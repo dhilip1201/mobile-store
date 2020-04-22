7ed1f55bdf525ba09adc35f98cb98f7aa0ef373b
dhilip1201 <dhilipbeece006@gmail.com>
2020-04-23 00:05:44 +0530

    Updates

---
+++ precache-manifest.ace78550f5ec94428dd97973458030f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469632aee9d925094c41ba39767b7317",
+    "url": "/mobile-store/index.html"
+  },
+  {
+    "revision": "e942e611f9f08728e711",
+    "url": "/mobile-store/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a874b929b8e4a025f033",
+    "url": "/mobile-store/static/css/main.0c545322.chunk.css"
+  },
+  {
+    "revision": "e942e611f9f08728e711",
+    "url": "/mobile-store/static/js/2.353ccc54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mobile-store/static/js/2.353ccc54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a874b929b8e4a025f033",
+    "url": "/mobile-store/static/js/main.ae8f4fe7.chunk.js"
+  },
+  {
+    "revision": "5fe3de0fdb37552bf0c7",
+    "url": "/mobile-store/static/js/runtime-main.2f5106e1.js"
+  },
+  {
+    "revision": "5005b889af213c79a5e4c40f50946ab0",
+    "url": "/mobile-store/static/media/logo.5005b889.svg"
+  }
+]);
